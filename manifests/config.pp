c48129587e7093e346784486db2c14f797ec5603
Davíð Örn Jóhannsson <davidoj@siminn.is>
2017-01-25 12:02:59 +0000

    adding CoreOS support

--- manifests/config.pp
+++ manifests/config.pp
@@ -60,6 +60,49 @@ class flannel::config {
         Exec['reload systemctl daemon for flannel']
       }
     }
+  } else if $::osfamily == 'CoreOS' {
+
+    file { '/etc/sysconfig/flanneld':
+      ensure  => file,
+      content => template("${module_name}/sysconfig/flanneld.erb"),
+      mode    => '0644',
+    }
+
+    if $::flannel::manage_docker {
+      $docker_dropin_ensure = 'file'
+      include ::docker
+      File['/etc/systemd/system/docker.service.d/flannel.conf'] ~> Service['flanneld'] ~> Service['docker']
+    } else {
+      $docker_dropin_ensure = 'absent'
+    }
+
+    file { '/etc/systemd/system/docker.service.d/flannel.conf':
+      ensure  => $docker_dropin_ensure,
+      content => template("${module_name}/service_flannel.conf"),
+      mode    => '0644',
+    } ~>
+    exec { 'reload systemctl daemon for flannel':
+      command     => '/bin/systemctl daemon-reload',
+      refreshonly => true,
+    }
+
+    if $::flannel::journald_forward_enable {
+      file { '/etc/systemd/system/flanneld.service.d':
+        ensure => 'directory',
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+      file { '/etc/systemd/system/flanneld.service.d/journald.conf':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template("${module_name}/journald.conf.erb"),
+      } ~>
+      Exec['reload systemctl daemon for flannel']
+    }
+
   } else {
     fail("Unsupported OS: ${::osfamily}")
   }
